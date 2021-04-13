6ea665f65b0d85cb531e3ec246fb2a731023f393
ggracis <gastongracis@gmail.com>
2021-04-13 13:43:22 -0300

    cambiando a PHP

---
+++ comercio_servicios.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-AR">
+
+<?php
+  include "head.html";
+  ?>
+
+<body class="stretched">
+  <!-- Document Wrapper
+	============================================= -->
+  <div id="wrapper" class="clearfix">
+    <!-- Header
+		============================================= -->
+    <?php
+  include "header.html";
+  ?>
+    <!-- #header end -->
+
+    <!-- Content
+		============================================= -->
+    <section id="content">
+      <div class="content-wrap pt-0 pb-0">
+
+
+
+        <!-- Bienvenida
+		============================================= -->
+    <?php include "bienvenida.html"; ?>
+
+        <!--  /Bienvenida
+		=============================================  -->
+
+
+
+        <!-- Contenido
+		============================================= -->
+        <div id="section-about" class="center page-section col-lg-12 col-md-12">
+
+          <div class="container clearfix">
+            <div class="heading-block center sec_top"><img src="imgs/sec/comercio_servicios.png">
+              <h2>Comercio <br>y Servicios</h2>
+            </div>
+            <h2 class="display-4 nott ls0 font-primary color text-left">Asistencia y acompañamiento al comercio
+              minorista nacional</h2>
+
+            <p class="lead mx-auto bottommargin" >En un difícil año para el sector comercial a
+              nivel federal, en el que el aislamiento y las medidas de restricción ante el avance de la pandemia
+              limitaron la actividad en todo el país, paralizando parcialmente su entramado, desde el sector Comercio y
+              Servicios de CAME impulsamos acciones y gestiones, tanto regionales como nacionales, orientadas a
+              preservar el tejido pyme y las fuentes de empleo que de él dependen.</p>
+
+            <div class="clear"></div>
+          </div>
+        </div>
+
+        <div class="container clearfix col-lg-12 col-md-12">
+          <div class="row justify-content-between">
+            <div class="col-lg-10 col-md-10 mb-0"> <h2>Logros de gestión:</h2></div>
+            <div class="clear"></div>
+       
+            <div class="col-lg-3 col-md-4 my-4">
+              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
+              <p class="text-muted">
+                Sanción de la Ley de Acreditación Inmediata de Pagos con Tarjetas de Débito.
+              </p>
+            </div>
+            <div class="col-lg-3 col-md-4 my-4">
+              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
+              <p class="text-muted">
+               Convenio de colaboración para desarrollar CCA con el Gobierno de la Provincia de Santa Fe, suscripto por CAME, AER, FSCCA, FECECO y el Ministerio de Producción de la provincia.
+              </p>
+            </div>
+            <div class="col-lg-3 col-md-4 my-4">
+              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
+              <p class="text-muted">
+                Acompañamiento a la Federación Económica de Tucumán (FET) y rechazo del accionar del Sindicato de Empleados de Comercio (SEOC) que imposibilitó aperturas en la provincia.              </p>
+            </div>
+            <div class="col-lg-3 col-md-4 my-4">
+              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
+              <p class="text-muted">
+               Apoyo para el lanzamiento de promociones de Fin de Año, para 87 CCA y entidades de 12 provincias. 
+                            </p>
+            </div>
+
+           
+
+       
+
+         
+          </div>
+        </div>
+
+        <div class="container clearfix">
+          <div class="row justify-content-center">
+            <div class="col-md-12">
+              <div class="row col-mb-30 mb-5">
+                <div class="col-lg-10 col-md-10 mb-0"> <h2>Acciones en números:</h2></div>
+                <div class="clear"></div>
+
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="10" data-refresh-interval="10" data-speed="1600">10</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">entidades y CCA</bold> recibieron asistencia para actualizar y potenciar plataformas digitales de fidelización de clientes.</p>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="2700" data-refresh-interval="10" data-speed="1600">2700</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">participantes</bold> en el XIX Foro Internacional sobre Ciudad, Comercio y Turismo, realizado en formato virtual.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="6" data-refresh-interval="10" data-speed="1600">6</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">campañas audiovisuales de concientización</bold> que destacan el rol del comercio en la comunidad y apelan a la responsabilidad sanitaria. </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="15" data-refresh-interval="10" data-speed="1600">15</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">ciudades</bold> de 6 provincias recibieron propuestas técnicas para proyectos de CCA en desarrollo.  </p>
+                    </div>
+                  </div>
+                </div>
+
+                
+                <div class="col-md-6">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="16" data-refresh-interval="10" data-speed="1600">16</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">ciudades</bold> de 7 provincias recibieron asistencia técnica para CCA ya establecidos.   </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="2" data-refresh-interval="10" data-speed="1600">2</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">coloquios internacionales:</bold> “Los Centros Comerciales Abiertos en cuarentena” (con 220 participantes) y “Los desafíos pospandemia para el Comercio Minorista y los CCA” (con 115 participantes).</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="4" data-refresh-interval="10" data-speed="1600">4</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">encuentros</bold> en el marco del ciclo de CCA Sustentables, iniciativa desarrollada junto con el Departamento de RSE de CAME. </p>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+  
+          </div>
+        </div>
+
+      
+
+      <!-- fin Contenido
+    ============================================= -->
+
+
+
+
+  </div>
+  </section>
+  <!-- #content end -->
+
+  <!-- Footer
+		============================================= -->
+    <?php
+  include "footer.html";
+  ?>
+  <!-- #footer end -->
+  </div>
+  <!-- #wrapper end -->
+
+  <!-- Go To Top
+	============================================= -->
+  <div id="gotoTop" class="icon-angle-up"></div>
+
+  <!-- JavaScripts
+	============================================= -->
+  <script src="js/jquery.js"></script>
+  <script src="js/plugins.min.js"></script>
+
+  <!-- Footer Scripts
+	============================================= -->
+  <script src="js/functions.js"></script>
+</body>
+
+</html>
---
+++ prologo.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-AR">
+
+  <?php
+  include "head.html";
+  ?>
+  
+
+<body class="stretched">
+  <!-- Document Wrapper
+	============================================= -->
+  <div id="wrapper" class="clearfix">
+    <!-- Header
+		============================================= -->
+    <?php
+  include "header.html";
+  ?>
+    <!-- #header end -->
+
+    <!-- Content
+		============================================= -->
+    <section id="content">
+      <div class="content-wrap pt-0 pb-0">
+
+        
+
+        <!-- Bienvenida
+		============================================= -->
+    <?php include "bienvenida.html"; ?>
+
+        <!--  /Bienvenida
+		=============================================  -->
+
+
+
+   
+    
+    
+       <!-- 
+    Contenido 5 Prologo 
+		============================================= -->
+    <div class="section m-0 p-0 testimonials clearfix">
+      <div class="container" style="padding: 120px 0">
+        <div class="heading-block border-bottom-0 center">
+          
+          <h2 class="nott font-weight-semibold">Prólogo</h2>
+         
+        </div>
+
+        <div
+        
+          class="bg-transparent border-0 shadow-none p-0 mx-auto text-left clearfix"
+          data-animate-in="fadeIn"
+          data-animate-out="fadeOut"
+          data-margin="24"
+          data-nav="false"
+          data-pagi="true"
+          data-items="1"
+          style="max-width: 740px"
+        >
+         
+            <div >
+              <p>
+                En un año sin precedentes en la historia argentina y mundial, signado por la pandemia que aún nos asola, la Confederación Argentina de la Mediana Empresa (CAME), en su rol histórico y capilar al frente de las entidades asociadas y de las pymes por estas representadas en todo el territorio nacional, comparte el Informe de gestión 2020, en el cual la convicción en su ejercicio y la condición estratégica del quehacer diario son banderas permanentes. 
+No es posible celebrar este año, lamentablemente; sin embargo, es dable señalar un carácter y una cualidad concretos de nuestras pymes: su resiliencia. Esa capacidad para adaptarse a las situaciones adversas con resultados, en ciertos casos, positivos ha sido el horizonte de muchas y lo que permitió su supervivencia en este marco. Asimismo, forma parte de un deseo y, por ende, de una voluntad de hacer a pesar del escenario inevitable del brote.
+En un ciclo que se clausura, el orgullo de encabezar CAME, con los valores que esta viste, hace mella en el futuro: ha sido un año distinto tanto institucional como socioeconómicamente por lo antedicho. En la primera arista, destacamos la unión y el coraje de las entidades que conforman y ponen su pasión al servicio de las pymes, constituyendo y encarnando un órgano colegiado, que brega por un bien superior a ellas: el desarrollo del entramado productivo y social, segunda arista que, con la misma convicción en la dignidad por el trabajo, defendemos a través de cada acto, en conocimiento pleno de lo que implica para la economía del país y para la sociedad en su conjunto.
+Por último, las vigas que sostienen CAME, sus sectores y secretarías, hacen su labor cotidiana, incansable, para que el andamiaje marche. A ellos también nuestro reconocimiento y, a quien lee, nuestra invitación a navegar por estas páginas que nos muestran en lo que hemos realizado y en aquello que aún está por construirse. 
+              </p>  
+
+           
+            </div>
+
+        </div>
+      </div>
+    </div>
+
+        </div>
+      </section>
+      <!-- #content end -->
+
+     	<!-- Footer
+		============================================= -->
+	  <?php
+  include "footer.html";
+  ?>
+  <!-- #footer end -->
+    </div>
+    <!-- #wrapper end -->
+
+    <!-- Go To Top
+	============================================= -->
+    <div id="gotoTop" class="icon-angle-up"></div>
+
+    <!-- JavaScripts
+	============================================= -->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.min.js"></script>
+
+    <!-- Footer Scripts
+	============================================= -->
+    <script src="js/functions.js"></script>
+  </body>
+</html>
