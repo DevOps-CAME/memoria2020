1165375dca88b1c4c7dd5d4dd5d2baa1e894d6ff
ggracis <gastongracis@gmail.com>
2021-04-13 16:20:46 -0300

    video

--- index.php
+++ index.php
@@ -28,22 +28,22 @@ include "header.html";
         <section class="overflow-hidden parallax" style="
         background-image: url('imgs/tapa.jpg');
         background-size: cover;
-        height: 90vh;
+        height: 55vh;
         width: 100%;" data-bottom-top="background-position:0px 200px;"
           data-top-bottom="background-position:0px -100px;">
 
 
           <div class=" d-flex align-items-center" style="
-        height: 50vh;
+        height: 55vh;
         width: 100%;">
-            <div class="container" style="margin-top: -6rem;">
+            <div class="container">
               <div class="row">
 
                 <div class="col-md-11 center">
 
 
-                  <h2 data-animate="fadeInUp" class="display-2 font-weight-semibold ls--4 mb-0">
-                    INFORME DE GESTIÓN </h2>
+                  <h1 data-animate="fadeInUp" class="display-2 font-weight-semibold ls--4 mb-0">
+                    INFORME DE GESTIÓN </h1>
                   <h2 data-animate="fadeInUp" data-delay="200" class="display-2 ls--6 mb-2">
                     2020
                   </h2>
@@ -53,88 +53,40 @@ include "header.html";
             </div>
           </div>
 
-
-          <div class="mb-1" data-animate="fadeInRight" data-delay="300">
-            <div class="shadow-sm hero-features bg-color dark shadow">
-              <div class="row">
-                <div class="col-md-12 mt-3 mt-md-0">
-                  <div class="feature-box fbox-plain fbox-sm fbox-dark mb-0">
-                    <div class="fbox-icon">
-
-                    </div>
-                    <div class="fbox-content" data-animate="fadeInRight" data-delay="400">
-
-                      <p class="text-white mb-0">
-                        “Apostamos por el país, como lo hemos hecho siempre, y lo seguiremos haciendo. Porque estamos
-                        convencidos de que el desarrollo de la pequeña y mediana industria, así como del turismo, el
-                        sector
-                        comercial, las economías regionales, la construcción, los parques industriales, los jóvenes
-                        empresarios y
-                        las mujeres que se dedican a la actividad, constituyen el programa de crecimiento vertebral que
-                        la
-                        nación
-                        demanda en esta inédita etapa”.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-md-12 mt-3 mt-md-0">
-                  <div class="feature-box fbox-plain fbox-sm fbox-dark mb-0">
-                    <div class="fbox-icon">
-
-                    </div>
-                    <div class="fbox-content" data-animate="fadeInRight" data-delay="450">
-                      <h3 class="text-white"></h3>
-                      <p class="text-white mb-0">
-                        “Tenemos la certeza de que la pandemia no acabará en 2021, y por ello reivindicamos el diálogo
-                        con
-                        todos los
-                        sectores y la genuina demanda de asistencia para que nuestras pymes, que representan más del 40
-                        %
-                        del PBI nacional
-                        y el 70 % del empleo registrado, puedan salir adelante”.
-                      </p>
-                      <h3 class="text-white"><i class="icon-line-circle-check"></i> Dr. Gerardo Díaz Beltrán</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-
-          <div class="container" style="margin-top: -6rem;padding-bottom: 2rem;">
-
-            <div class="row">
-              <div class="col-md-7">
-                <h2 class="display-4 font-weight-semibold ls--2">
-                  Representamos a 1491
-                  <span class="text-rotater" data-separator="|" data-rotate="fadeIn" data-speed="2000">
-                    <span class="t-rotate">Federaciones|Cámaras|Centros industriales|Parques
-                      Industriales|Comercio y Servicios|Turismo|Economías
-                      Regionales|Construcción|Jóvenes Empresarios|Mujeres
-                      Empresarias|</span>
-                  </span>
-                  </br>
-                </h2>
-              </div>
-              <div class="col-md-5">
-                <p class="lead text-muted">
-                  <b>de todo el país</b> que agrupan a más 600.000 empresas pymes y dan trabajo a
-                  4.200.000 personas.
-                </p>
-              </div>
-            </div>
-          </div>
-
         </section>
         <!--  /Bienvenida
 		=============================================  -->
 
  
+<section class="section m-0 p-5">
+<div class="container">
 
+<div class="row">
+<div class="col-md-6">
+<div class="video-wrap">
+						<video poster="imgs/video.jpg" preload="auto" loop autoplay muted>
+							<source src='imgs/video.mp4' type='video/mp4' />
+							<!-- <source src='images/videos/explore.webm' type='video/webm' /> -->
+						</video>
+					</div>
+</div>  
+<div class="col-md-6">
+    <h2 class="display-4 font-weight-semibold ls--2">
+      Representamos a 1491
+      <span class="text-rotater" data-separator="|" data-rotate="fadeIn" data-speed="2000">
+        <span class="t-rotate">Federaciones|Cámaras|Centros industriales|Parques Industriales|Comercio y Servicios|Turismo|Economías Regionales|Construcción|Jóvenes Empresarios|Mujeres Empresarias</span>
+      </span>
+      </br>
+    </h2>
+
+    <p class="lead text-muted">
+      <b>de todo el país</b> que agrupan a más 600.000 empresas pymes y dan trabajo a
+      4.200.000 personas.
+    </p>
+  </div>
+</div>
+</div>
+</section>
 
 
 
@@ -159,33 +111,33 @@ include "header.html";
                     <div class="counter color font-weight-semibold">
                       <span data-from="1" data-to="150" data-refresh-interval="2" data-speed="600"></span>+
                     </div>
-                    <h5 class="mt-0 font-weight-medium">Gestiones realizadas para morigerar impacto de la pandemia</h5>
+                    <h4 class="mt-0 font-weight-medium">Gestiones realizadas para morigerar impacto de la pandemia</h4>
                   </div>
 
                   <div class="col-6 col-sm-6 mb-4">
                     <div class="counter color font-weight-semibold">
                       <span data-from="1" data-to="80" data-refresh-interval="11" data-speed="900"></span>+
                     </div>
-                    <h5 class="mt-0 font-weight-medium">Personas capacitadas a través de la oferta de cursos regular de
+                    <h4 class="mt-0 font-weight-medium">Personas capacitadas a través de la oferta de cursos regular de
                       CAME Educativa, en relación con 2019, impulsando la modernización de las pymes, clave en el nuevo
-                      contexto mundial.</h5>
+                      contexto mundial.</h4>
                   </div>
 
                   <div class="col-6 col-sm-6 mb-4">
                     <div class="counter color font-weight-semibold">
                       <span data-from="1" data-to="53" data-refresh-interval="3" data-speed="1000"></span>+
                     </div>
-                    <h5 class="mt-0 font-weight-medium">Nuevos seminarios web (webinars) para satisfacer la demanda de
-                      formación y comunicación de nuestras entidades adheridas.</h5>
+                    <h4 class="mt-0 font-weight-medium">Nuevos seminarios web (webinars) para satisfacer la demanda de
+                      formación y comunicación de nuestras entidades adheridas.</h4>
                   </div>
 
                   <div class="col-6 col-sm-6 mb-4">
                     <div class="counter color font-weight-semibold">
                       <span data-from="1" data-to="15" data-refresh-interval="2" data-speed="1500"></span>+
                     </div>
-                    <h5 class="mt-0 font-weight-medium">Reuniones de Consejo Directivo, para fortalecer la gremial
+                    <h4 class="mt-0 font-weight-medium">Reuniones de Consejo Directivo, para fortalecer la gremial
                       empresaria y generar sinergias que resulten en propuestas concretas para la defensa del sector.
-                    </h5>
+                    </h4>
                   </div>
                 </div>
 
@@ -260,7 +212,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-line2-cup color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-leaf color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Economías Regionales</h4>
               <p class="text-muted">
                 Como miembro permanente de la Comisión Nacional de Trabajo Agrario (CNTA) y la Comisión Nacional
@@ -276,7 +228,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-line2-screen-desktop color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-line-trending-up color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Comercio</h4>
               <p class="text-muted">
                 Participamos de las comisiones de Comercio y de Finanzas de la Honorable Cámara de Diputados
@@ -289,7 +241,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-line2-lock color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-money-bill-wave-alt color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Financiamiento</h4>
               <p class="text-muted">
                 Recibimos más de 2897 consultas de entidades, pymes y emprendedores de todo el país en relación con los
@@ -301,7 +253,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-fingerprint color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-industry color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Industria</h4>
               <p class="text-muted">
                 Participamos en las reuniones mensuales de la Comisión Pymes del Ente Regulador del Gas (ENARGAS),
@@ -314,7 +266,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-line2-printer color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-plane1 color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Turismo</h4>
               <p class="text-muted">
                 Presentamos un plan integral y federal de promoción y recuperación de la actividad,
@@ -326,7 +278,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-couch color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-line2-user-female color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Mujeres </h4>
               <p class="text-muted">
                 En un esfuerzo institucional transversal, CAME adhirió a los Principios para el
@@ -340,7 +292,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-line-clock color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-people-arrows color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Rondas de Negocios</h4>
               <p class="text-muted">
                 En 2020, de manera virtual, se 7 Rondas de Negocios, en las que participaron 386 empresas,
@@ -352,7 +304,7 @@ include "header.html";
             <!-- Feature Box
 						============================================= -->
             <div class="col-lg-4 col-md-6 my-4">
-              <i class="icon-playstation color ml-0 mb-3 i-plain d-block float-none"></i>
+              <i class="icon-ship color ml-0 mb-3 i-plain d-block float-none"></i>
               <h4 class="mb-3">Exportación</h4>
               <p class="text-muted">
                 En el 2020, CAME emitió 11.084 Certificados de Origen, un 30% más respecto del año anterior,
@@ -373,60 +325,61 @@ include "header.html";
         <div class="section bg-transparent clearfix" style="padding: 80px 0">
           <div class="container">
             <div class="row justify-content-between align-items-center">
-              <div class="col-lg-8 col-md-10 offset-lg-2 offset-md-1 bottommargin-lg">
+           
+
+          <div class="fslider testimonial testimonial-full bottommargin" data-animation="fade" data-arrows="false">
+						<div class="flexslider">
+							<div class="slider-wrap">
+
+								<div class="slide">
+									<div class="testi-image">
+                  <a href="#"><img src="imgs/GerardoDiazBeltran.jpg" alt="Dr. Gerardo Díaz Beltrán"></a>
+									</div>
+									<div class="testi-content">
+										<p>Apostamos por el país, como lo hemos hecho siempre, y lo seguiremos haciendo.
+                      Porque estamos convencidos de que el desarrollo de la pequeña y mediana industria,
+                      así como del turismo, el sector comercial, las economías regionales, la construcción,
+                      los parques industriales, los jóvenes empresarios y las mujeres que se dedican a la actividad,
+                      constituyen el programa de crecimiento vertebral que la nación demanda en esta inédita etapa</p>
+										<div class="testi-meta">
+                    Dr. Gerardo Díaz Beltrán
+											<span>Presidente CAME</span>
+										</div>
+									</div>
+								</div>
+
+								<div class="slide">
+									<div class="testi-image">
+										<a href="#"><img src="imgs/GerardoDiazBeltran.jpg" alt="Dr. Gerardo Díaz Beltrán"></a>
+									</div>
+									<div class="testi-content">
+										<p>Tenemos la certeza de que la pandemia no acabará en 2021, y por ello reivindicamos el diálogo con
+                      todos los sectores y la genuina demanda de asistencia para que nuestras pymes, que representan más
+                      del
+                      40 % del PBI nacional y el 70 % del empleo registrado, puedan salir adelante</p>
+										<div class="testi-meta">
+										Dr. Gerardo Díaz Beltrán
+											<span>Presidente CAME</span>
+										</div>
+									</div>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+
+          <div class="row">
+          <div class="col-lg-6 bottommargin-lg">
                 <a href="https://vimeo.com/518728139" data-lightbox="iframe"
                   class="play-video position-relative">
                   <img data-animate="fadeInLeft" data-delay="2600" src="imgs/video_vimeo.png" alt="Video Image"  class="fadeInLeft animated rounded-lg shadow-lg" />
                   <i class="icon-play"></i>
                 </a>
               </div>
-              <div class="col-lg-5 col-md-6 mt-0 mt-md-4">
-                <div class="feature-box fbox-plain fbox-sm">
-                  <div class="fbox-icon">
-                    <i class="icon-line2-magic-wand"></i>
-                  </div>
-                  <div class="fbox-content">
-                    <h3>Dr. Gerardo Díaz Beltrán</h3>
-                    <p>
-                      “Apostamos por el país, como lo hemos hecho siempre, y lo seguiremos haciendo.
-                      Porque estamos convencidos de que el desarrollo de la pequeña y mediana industria,
-                      así como del turismo, el sector comercial, las economías regionales, la construcción,
-                      los parques industriales, los jóvenes empresarios y las mujeres que se dedican a la actividad,
-                      constituyen el programa de crecimiento vertebral que la nación demanda en esta inédita etapa”.
-                    </p>
-                  </div>
-                </div>
+              
+              
 
-                <div class="feature-box fbox-plain fbox-sm">
-                  <div class="fbox-icon">
-                    <i class="icon-line2-clock"></i>
-                  </div>
-                  <div class="fbox-content">
-                    <h3>Dr. Gerardo Díaz Beltrán</h3>
-                    <p>
-                      “Tenemos la certeza de que la pandemia no acabará en 2021, y por ello reivindicamos el diálogo con
-                      todos los sectores y la genuina demanda de asistencia para que nuestras pymes, que representan más
-                      del
-                      40 % del PBI nacional y el 70 % del empleo registrado, puedan salir adelante”.
-                    </p>
-                  </div>
-                </div>
-
-                <div class="feature-box fbox-plain fbox-sm mb-0">
-                  <div class="fbox-icon">
-                    <i class="icon-line2-speech"></i>
-                  </div>
-                  <div class="fbox-content">
-                    <h3>Great Community</h3>
-                    <p>
-                      Proactively evolve user-centric paradigms rather than
-                      exceptional core. Authoritatively brand unique.
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-5 col-md-6 mt-5 mt-md-0">
+              <div class="col-lg-6 ">
                 <h2 class="ls0">Get One Day Free Trial.</h2>
                 <p class="text-muted mb-5" style="font-size: 18px">
                   Enthusiastically embrace diverse e-markets after sustainable
@@ -442,6 +395,8 @@ include "header.html";
                     Us</a>
                 </div>
               </div>
+</div>
+
             </div>
           </div>
         </div>
