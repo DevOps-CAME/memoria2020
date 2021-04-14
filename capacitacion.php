4fe3992b1317e88daf7707ab35f93da2e9603290
Enzosalvo <enzosalvo1986@gmail.com>
2021-04-14 15:51:51 -0300

    capacitacion completado

--- capacitacion.php
+++ capacitacion.php
@@ -36,12 +36,14 @@
             <div class="heading-block center sec_top"><img src="imgs/sec/capacication.png">
               <h2>capacitación    </h2>
             </div>
-            <h2 class="display-4 nott ls0 font-primary color text-left">Gestión para fortalecer el desarrollo de Parques Industriales en todo el país
+            <h2 class="display-4 nott ls0 font-primary color text-left">Formación para la modernización de nuestros asociados y sus pymes
             </h2>
 
-            <p class="lead mx-auto bottommargin" >A los efectos de promover a un sector pujante del aparato productivo nacional que contribuye al desarrollo económico y social del país, a través de la representación de nuestra asociada, la Federación Argentina de Parques Industriales, asistimos brindando fortalecimiento institucional, apoyo técnico y acompañamiento de gestión para la implementación de programas, proyectos e instrumentos, que motoricen su desarrollo, así como el de las empresas que los componen y el de las localidades en las que estas unidades productivas se radican.
+            <p class="lead mx-auto bottommargin" >Más de 245.000 personas fueron capacitadas durante 2020 a través de la oferta de capacitación que brinda CAME.</p>
+            <p class="lead mx-auto bottommargin" >Creció un 80% la cantidad de personas que se capacitaron a distancia en nuestra oferta regular, en relación a 2019.</p>
+            <p class="lead mx-auto bottommargin" >Registramos un incremento del 72% en los egresos de las Diplomaturas de nuestra Escuela de Negocios, en relación con 2019.</p>
+            <p class="lead mx-auto bottommargin" >Diseñamos 53 seminarios web para brindar a todas las entidades adheridas a CAME ante la imposibilidad de dictar actividades presenciales.</p>
 
-            </p>
 
             <div class="clear"></div>
           </div>
@@ -59,9 +61,9 @@
                   <div class="feature-box fbox-small fbox-effect">
                     
                     <div class="fbox-content">
-                      <div class="counter color"><span data-from="1" data-to="16" data-refresh-interval="10" data-speed="1600">16</span> </div>
+                      <div class="counter color"><span data-from="1" data-to="40" data-refresh-interval="5" data-speed="1600">16</span> </div>
                      
-                      <p><bold class="display-5 color font-weight-medium">reuniones, virtuales y presenciales, del sector</bold> </p>
+                      <p><bold class="display-5 color font-weight-medium">Videoconferencias</bold> diarias con más de 5.200 participantes</p>
                     </div>
                   </div>
                 </div>
@@ -70,9 +72,9 @@
                   <div class="feature-box fbox-small fbox-effect">
                     
                     <div class="fbox-content">
-                      <div class="counter color"><span data-from="1" data-to="7" data-refresh-interval="10" data-speed="1600">7</span> </div>
+                      <div class="counter color"><span data-from="1" data-to="3" data-refresh-interval="10" data-speed="1600">7</span> </div>
                      
-                      <p><bold class="display-5 color font-weight-medium">encuentros regionales: </bold> un encuentro presencial en la Región NEA y 6 virtuales, para las regiones NEA, NOA, Centro, Patagonia Sur, Norte y Cuyo
+                      <p><bold class="display-5 color font-weight-medium">Programas específicos</bold> sobre “Cómo implementar el comercio electrónico en la Pyme”, “Cómo gestionar las ventas del Negocio con herramientas digitales” y “Cómo implementar una oficina virtual” donde participaron en total más de 600 personas.
                       </p>
                     </div>
                   </div>
@@ -82,15 +84,26 @@
                   <div class="feature-box fbox-small fbox-effect">
                     
                     <div class="fbox-content">
-                      <div class="counter color"><span data-from="1" data-to="58" data-refresh-interval="10" data-speed="1600">58</span> </div>
+                      <div class="counter color"><span data-from="1" data-to="245" data-refresh-interval="10" data-speed="1600">58</span> </div>
                      
-                      <p><bold class="display-5 color font-weight-medium">publicaciones de interés,</bold> entre las que se cuentan notas en la Revista Comerciar, circulares institucionales y publicaciones en redes sociales
+                      <p><bold class="display-5 color font-weight-medium">videoconferencias</bold> a solicitud de las Entidades de la red CAME que representaron capacitar a más de 9.000 personas en todo el país.
                       </p>
                     </div>
                   </div>
                 </div>
 
-               
+                
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="2100" data-refresh-interval="300" data-speed="1600">7</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">actividades</bold> de capacitación realizadas por las entidades adheridas a CAME, con una participación de 97.450 personas. 
+                      </p>
+                    </div>
+                  </div>
+                </div>
                
 
                
@@ -104,52 +117,41 @@
 
         <div class="container clearfix col-lg-12 col-md-12">
           <div class="row justify-content-between">
-            <div class="col-lg-10 col-md-10 mb-0"> <h2>Asimismo, llevamos a cabo acciones y encuentros estratégicos para atender la situación de nuestras entidades asociadas:</h2></div>
+            <div class="col-lg-10 col-md-10 mb-0"> <h4>Durante 2020, CAME ha brindado directa e indirectamente un total de 2500 actividades de capacitación a través de la plataforma Zoom, con más de 115.000 participantes de todo el país. Sumando ello a los 130.000 participantes de los cursos online de CAME Educativa, resultaron más de 245.000 personas capacitadas durante el año a través de la gestión de CAME.</h4></div>
             <div class="clear"></div>
-       
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-                Reunión entre el Ministro de Industria, Trabajo y Comercio de la Provincia de Corrientes y las autoridades de la Federación Económica de Corrientes, la Asociación de la Producción, Industria y Comercio de Corrientes y la Federación Empresaria de Corrientes.
+            <div class="col-lg-5 col-md-6 mt-0 mt-md-4">
+                <div class="feature-box fbox-plain fbox-sm">
+                  <div class="fbox-icon">
+                    <i class="icon-line2-magic-wand"></i>
+                  </div>
+                  <div class="fbox-content">
+                    <h3>ALIANZAS Y ACTIVIDADES CON EL SECTOR PÚBLICO</h3>
+                    <p>
+                    • Integramos la Red de la Economía del Conocimiento para las habilidades del futuro, creada por la Subsecretaría de Economía del Conocimiento del Ministerio de Desarrollo Productivo de la Nación. El objetivo general es impulsar un espacio de articulación entre los distintos sectores de la Economía del Conocimiento y la Industria 4.0 a los fines de debatir y planificar políticas públicas que promuevan la capacitación y actualización de conocimientos, saberes, habilidades y aptitudes de recursos humanos para el desarrollo económico y la reactivación económica.</br>
+                    • CAME fue designada para formar parte del Consejo Nacional de Calidad de la Educación por parte del ministro de Educación de la Nación, Nicolás Trotta. 
 
-              </p>
-            </div>
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-                Audiencia con el Dr. Gerardo Gabriel Girón, Subsecretario de Articulación Territorial, y el Sr. Manuel Luaces, jefe de Agencia Territorial San Martin, ambos funcionarios del Ministerio de Trabajo de la Nación, en el marco de la Presentación de Programas: Acciones de Entrenamiento para el Trabajo (EPT) y Programa de Inserción Laboral (PIL) modalidad común / PIL Empalme.
+                    </p>
+                  </div>
+                </div>
 
-              </p>
-            </div>
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-                Organización y participación en la reunión técnica “Programa Desarrollo de Parques Industriales” junto a Red Parques. Se contó con la participación de la Lic. Mercedes La Gioiosa, directora nacional de Desarrollo Regional dependiente del Ministerio de Desarrollo Productivo Nacional.
-            </div>
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-                Firma de acuerdo junto a “Red Parques” a fin de fortalecer el sector.
-                            </p>
-            </div>
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-                Presentación de la reglamentación y modelos de formularios del Programa Nacional Para el Desarrollo de Parques/ reunión técnica <bold class="color">“Programa Desarrollo de Parques Industriales”, creado en el Decreto 716/2020.</bold>
-                            </p>
-            </div>
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-                Stand de FAPI en la 6ta. Exposición de Parques Industriales, EPIBA 2020 
-                            </p>
-            </div>
-            <div class="col-lg-4 col-md-4 my-4">
-              <i class="icon-line-check-circle color ml-0 mb-4 i-plain d-block float-none"></i>
-              <p class="text-muted">
-               Coorganización y participación del panel “Desarrollo provincial de parques industriales” en EPIBA 2020, en el cual se abordó la temática de la planificación y desarrollo de agrupamientos como herramienta valiosa para el impulso de políticas públicas de los gobiernos provinciales. Presencia de Gobernadores de Mendoza y Córdoba.
-                            </p>
-            </div>
+                <div class="feature-box fbox-plain fbox-sm">
+                  <div class="fbox-icon">
+                    <i class="icon-line2-clock"></i>
+                  </div>
+                  <div class="fbox-content">
+                    <h3>ALIANZAS Y ACTIVIDADES CON EL SECTOR PRIVADO</h3>
+                    <p> Renovamos el convenio con Microsoft Argentina enfocado principalmente en colaborar con las pymes de la red CAME en el proceso de transformación digital que se vio acelerado en 2020. Ello se materializó a través de dos aspectos concretos: </br>
+
+•	Infraestructura: acercar productos de Microsoft a las Pymes de la red CAME, con beneficios y descuentos, para poder implementar mejoras que les permitan adaptarse a las nuevas exigencias; </br>
+•	Capacitación: realización de diferentes actividades de capacitación. En ese sentido participamos en conjunto con Microsoft de dos importantes eventos: “La transformación digital de las Pymes argentinas”, y “Reimaginando el futuro de las Pymes”.
+
+                    </p>
+                  </div>
+                </div>
+
+                
+
+
 
 
             
