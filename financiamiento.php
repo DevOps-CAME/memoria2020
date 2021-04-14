de639812395e7df7915a832bd713c574bd130780
Enzosalvo <enzosalvo1986@gmail.com>
2021-04-14 17:26:25 -0300

    ..

--- financiamiento.php
+++ financiamiento.php
@@ -41,19 +41,7 @@
               <h2>Financiamiento y competitividad </h2>
               <div class="clear"></div>
             </div>
-            <h2 class="display-4 nott ls0 font-primary color text-left">FINANCAME en cifras 2020</h2>
-
-            <p class="lead mx-auto bottommargin" >Durante el 2020, desde FINANCAME hemos alcanzado los siguientes resultados en cifras: </br>
-2897 Consultas atendidas de entidades, pymes y emprendedores de todo el país</br>
-280 Nuevos Proyectos de pymes y emprendedores acercados a nuestra Plataforma online</br>
-3683 Reproducciones en Youtube de nuestras Videoconferencias, Talleres, Encuentros interactivos y Consultorios Regionales</br>
-30 Mentorías virtuales personalizadas de especialistas y pymes a emprendedores en el marco del Programa Comunidad de Mentores, ejecutado en conjunto con el Ministerio de Desarrollo Productivo de la Nación. </br>
-Más de 100 consultas recibidas de emprendedores acerca del Programa PAC Reactivación Productiva de Aportes No Reembolsables (ANR). 10 Proyectos seleccionados, formulados y presentados, por una solicitud total de $8.484.908,91</br>
-23 Encuentros Interactivos y Talleres temáticos realizados virtualmente y 8 Consultorios Regionales que nos acercaron a todas las entidades, pymes y emprendedores de la Red CAME, trabajando en conjunto con otros sectores y secretarías de CAME. </br>
-             
-              
-              </p>
-
+            
               <h2 class="display-4 nott ls0 font-primary color text-left">FINANCAME: Principales Objetivos – Planificación 2021</h2>
 
               <p class="lead mx-auto bottommargin" >
@@ -69,6 +57,92 @@ Más de 100 consultas recibidas de emprendedores acerca del Programa PAC Reactiv
 
 
             <div class="clear"></div>
+          </div>
+        </div>
+
+        <div class="container clearfix">
+          <div class="row justify-content-center">
+            <div class="col-md-12">
+              <div class="row col-mb-30 mb-5">
+                <div class="col-lg-10 col-md-10 mb-0"> <h2>Durante el 2020, desde FINANCAME hemos alcanzado los siguientes resultados en cifras: </h2></div>
+                <div class="clear"></div>
+
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="2897" data-refresh-interval="300" data-speed="1600">2897</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">Consultas atendidas </bold> de entidades, pymes y emprendedores de todo el país</p>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="280" data-refresh-interval="30" data-speed="1600">280</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">Nuevos Proyectos</bold> de pymes y emprendedores acercados a nuestra Plataforma online </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="3683" data-refresh-interval="400" data-speed="1600">3683</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">Reproducciones</bold> en Youtube de nuestras Videoconferencias, Talleres, Encuentros interactivos y Consultorios Regionales
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="30" data-refresh-interval="50" data-speed="1600">30</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">Mentorías virtuales personalizadas </bold> de especialistas y pymes a emprendedores en el marco del Programa Comunidad de Mentores, ejecutado en conjunto con el Ministerio de Desarrollo Productivo de la Nación. </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="100" data-refresh-interval="5" data-speed="1600">+ 100</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">consultas recibidas de emprendedores </bold> acerca del Programa PAC Reactivación Productiva de Aportes No Reembolsables (ANR). 10 Proyectos seleccionados, formulados y presentados, por una solicitud total de $8.484.908,91 </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-4">
+                  <div class="feature-box fbox-small fbox-effect">
+                    
+                    <div class="fbox-content">
+                      <div class="counter color"><span data-from="1" data-to="23" data-refresh-interval="2" data-speed="1600">23</span> </div>
+                     
+                      <p><bold class="display-5 color font-weight-medium">Encuentros Interactivos y Talleres temáticos </bold> realizados virtualmente y 8 Consultorios Regionales que nos acercaron a todas las entidades, pymes y emprendedores de la Red CAME, trabajando en conjunto con otros sectores y secretarías de CAME.  </p>
+                    </div>
+                  </div>
+                </div>
+
+
+               
+
+               
+                
+              </div>
+            </div>
+  
           </div>
         </div>
         
