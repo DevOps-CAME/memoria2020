551247837a81a49e2e477e892367e64bf2a93875
Enzosalvo <enzosalvo1986@gmail.com>
2021-04-14 17:19:26 -0300

    ..

--- financiamiento.php
+++ financiamiento.php
@@ -43,13 +43,13 @@
             </div>
             <h2 class="display-4 nott ls0 font-primary color text-left">FINANCAME en cifras 2020</h2>
 
-            <p class="lead mx-auto bottommargin" >Durante el 2020, desde FINANCAME hemos alcanzado los siguientes resultados en cifras: 
-2897 Consultas atendidas de entidades, pymes y emprendedores de todo el país
-280 Nuevos Proyectos de pymes y emprendedores acercados a nuestra Plataforma online
-3683 Reproducciones en Youtube de nuestras Videoconferencias, Talleres, Encuentros interactivos y Consultorios Regionales
-30 Mentorías virtuales personalizadas de especialistas y pymes a emprendedores en el marco del Programa Comunidad de Mentores, ejecutado en conjunto con el Ministerio de Desarrollo Productivo de la Nación. 
-Más de 100 consultas recibidas de emprendedores acerca del Programa PAC Reactivación Productiva de Aportes No Reembolsables (ANR). 10 Proyectos seleccionados, formulados y presentados, por una solicitud total de $8.484.908,91
-23 Encuentros Interactivos y Talleres temáticos realizados virtualmente y 8 Consultorios Regionales que nos acercaron a todas las entidades, pymes y emprendedores de la Red CAME, trabajando en conjunto con otros sectores y secretarías de CAME. 
+            <p class="lead mx-auto bottommargin" >Durante el 2020, desde FINANCAME hemos alcanzado los siguientes resultados en cifras: </br>
+2897 Consultas atendidas de entidades, pymes y emprendedores de todo el país</br>
+280 Nuevos Proyectos de pymes y emprendedores acercados a nuestra Plataforma online</br>
+3683 Reproducciones en Youtube de nuestras Videoconferencias, Talleres, Encuentros interactivos y Consultorios Regionales</br>
+30 Mentorías virtuales personalizadas de especialistas y pymes a emprendedores en el marco del Programa Comunidad de Mentores, ejecutado en conjunto con el Ministerio de Desarrollo Productivo de la Nación. </br>
+Más de 100 consultas recibidas de emprendedores acerca del Programa PAC Reactivación Productiva de Aportes No Reembolsables (ANR). 10 Proyectos seleccionados, formulados y presentados, por una solicitud total de $8.484.908,91</br>
+23 Encuentros Interactivos y Talleres temáticos realizados virtualmente y 8 Consultorios Regionales que nos acercaron a todas las entidades, pymes y emprendedores de la Red CAME, trabajando en conjunto con otros sectores y secretarías de CAME. </br>
              
               
               </p>
@@ -57,10 +57,10 @@ Más de 100 consultas recibidas de emprendedores acerca del Programa PAC Reactiv
               <h2 class="display-4 nott ls0 font-primary color text-left">FINANCAME: Principales Objetivos – Planificación 2021</h2>
 
               <p class="lead mx-auto bottommargin" >
-              Para este nuevo año 2021, desde nuestro Departamento perseguiremos los siguientes objetivos principales y acciones vinculadas: 
-1. Profundizar el trabajo de CAME INCUBA hacia emprendedores: Comunidad de Mentores 2021 y su vínculo con entidades; Participación en ciclo de videos CAME “Saldremos Adelante” y nuevos Instagram Live; Desarrollo de contenidos de Pre Incubación para la Red de entidades CAME; Organización de una Feria – Expo virtual para emprendedores; Convenio de Colaboración Mutua con organizaciones de inversiones privadas (inversores ángeles) en emprendimientos. 
-2. Continuar trabajando en el asesoramiento técnico acerca del financiamiento disponible y seguimiento de proyectos presentados y en ejecución: Organización de un Encuentro Federal FINANCAME 2021 para dar continuidad a los Regionales 2020; Lanzamiento de un “Programa de Educación Financiera FINANCAME” que otorgue herramientas prácticas y ejecutemos junto con organismos públicos y especialistas; Realización de nuevos "Talleres interactivos FINANCAME 2021" orientados a actividades prácticas para luego aplicar en los negocios.  
-3. Desarrollar una nueva edición (la cuarta) del Índice estadístico IMAF FINANCAME 2021 para obtener información cuantitativa que fortalezca nuestra Planificación de actividades 2021. 
+              Para este nuevo año 2021, desde nuestro Departamento perseguiremos los siguientes objetivos principales y acciones vinculadas: </br>
+1. Profundizar el trabajo de CAME INCUBA hacia emprendedores: Comunidad de Mentores 2021 y su vínculo con entidades; Participación en ciclo de videos CAME “Saldremos Adelante” y nuevos Instagram Live; Desarrollo de contenidos de Pre Incubación para la Red de entidades CAME; Organización de una Feria – Expo virtual para emprendedores; Convenio de Colaboración Mutua con organizaciones de inversiones privadas (inversores ángeles) en emprendimientos. </br>
+2. Continuar trabajando en el asesoramiento técnico acerca del financiamiento disponible y seguimiento de proyectos presentados y en ejecución: Organización de un Encuentro Federal FINANCAME 2021 para dar continuidad a los Regionales 2020; Lanzamiento de un “Programa de Educación Financiera FINANCAME” que otorgue herramientas prácticas y ejecutemos junto con organismos públicos y especialistas; Realización de nuevos "Talleres interactivos FINANCAME 2021" orientados a actividades prácticas para luego aplicar en los negocios.  </br>
+3. Desarrollar una nueva edición (la cuarta) del Índice estadístico IMAF FINANCAME 2021 para obtener información cuantitativa que fortalezca nuestra Planificación de actividades 2021. </br>
 
 
 
