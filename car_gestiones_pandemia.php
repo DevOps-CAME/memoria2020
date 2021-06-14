02c3769113ae6073809e0424c386fc85fddb5fb2
ggracis <gastongracis@gmail.com>
2021-06-14 15:18:47 -0300

    retoques

--- car_gestiones_pandemia.php
+++ car_gestiones_pandemia.php
@@ -1,6 +1,61 @@
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-body">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel">LA FALTA DE COLABORACIÓN DE LOS BANCOS ESTÁ MATANDO A LAS
+                        PYMES</h4>
+
+                </div>
+                <div class="testimonial ml-4 mr-4 mb-5" style="border: 1px solid #0293d6;">
+
+                    <div class="testi-content">
+                        <div class="col-md-5 float-right">
+                            <img src="imgs/gest_bcra/GDB_BCRA_01.jpg" alt="Gerardo Díaz Beltrán"
+                                style="border-radius: 5px;">
+                        </div>
+                        <p>Esta pandemia dio visibilidad a los problemas
+                            estructurales de las pymes y la sociedad. Es el
+                            momento de trabajar regionalmente para potenciar las
+                            oportunidades. Debemos formar una mesa de diálogo
+                            permanente. De nosotros depende el futuro en los
+                            países de la región</p>
+                        <div class="col-md-12 float-left testi-meta">
+                            Gerardo Díaz Beltrán
+                            <span>Presidente de la Confederación Argentina de la Mediana
+                                Empresa
+                                (CAME)</span>
+                        </div>
+                        <a href="http://www.redcame.org.ar/novedades/8026/la-falta-de-colaboracin-de-los-bancos-est-matando-a-las-pymes"
+                            target="_blank"
+                            class="button button-small button-circle button-border button-blue text-right">Ver
+                            más<i class="icon-circle-arrow-right"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 <div id="car_gestiones_pandemia" class="owl-carousel portfolio-carousel carousel-widget" data-pagi="true"
     data-items-xs="1" data-items-sm="2" data-items-md="4" data-items-lg="4">
-    <div class="portfolio-item">
+
+    <div class="portfolio-item-bcra" style="cursor: pointer" data-toggle="modal" data-target=".bs-example-modal-lg">
+        <div class="portfolio-image">
+
+            <img src="imgs/gest_bcra/GDB_BCRA_01.jpg"
+                alt="La falta de colaboración de los bancos está matando a las pymes" />
+
+        </div>
+        <div class="portfolio-desc">
+            <h4 class="car_titulo color">La falta de colaboración de los bancos está matando a las pymes</h4>
+
+
+        </div>
+    </div>
+
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10367/historica-jornada-internacional-de-came-junto-a-adm-en-el-ii-encuentro-iberoamericano-de-convergencia-empresarial"
                 target="_blank">
@@ -8,149 +63,123 @@
             </a>
             <div class="portfolio-desc">
                 <p class="car_titulo">
-                    II Encuentro Iberoamericano de Convergencia Empresarial “Pandemia:
-                    crisis y desafíos”
+                    <a href="http://www.redcame.org.ar/novedades/10367/historica-jornada-internacional-de-came-junto-a-adm-en-el-ii-encuentro-iberoamericano-de-convergencia-empresarial"
+                        target="_blank">II Encuentro Iberoamericano de Convergencia Empresarial “Pandemia: crisis y
+                        desafíos”</a>
                 </p>
             </div>
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10367/historica-jornada-internacional-de-came-junto-a-adm-en-el-ii-encuentro-iberoamericano-de-convergencia-empresarial"
                 target="_blank">
-                <img src="imgs/gest_pand/01.jpg" alt="" />
+                <img src="imgs/gest_pand/01.jpg"
+                    alt="Reunión de dirigentes representantes de cámaras del Norte Grande" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                Dirigentes empresarios representantes de las cámaras del Norte
-                argentino, adheridas a la Confederación Argentina de la Mediana Empresa
-                (CAME), junto al presidente de la entidad, Gerardo Díaz Beltrán, se
-                reunieron con el presidente pro tempore del Consejo Regional del Norte
-                Grande y gobernador del Chaco, Jorge Capitanich, a fin de intercambiar
-                propuestas vinculadas al sector pyme regional.
+                <a href="http://www.redcame.org.ar/novedades/10367/historica-jornada-internacional-de-came-junto-a-adm-en-el-ii-encuentro-iberoamericano-de-convergencia-empresarial"
+                    target="_blank">Reunión de dirigentes representantes de cámaras del Norte Grande</a>
             </p>
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/9794/came-participo-de-la-reunion-la-cancilleria-como-promotora-de-las-exportaciones-argentinas"
                 target="_blank">
-                <img src="imgs/gest_pand/02.jpg" alt="" />
+                <img src="imgs/gest_pand/02.jpg"
+                    alt="Reunión La Cancillería como promotora de las exportaciones argentinas" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                Participamos de la reunión de trabajo organizada por la Fundación
-                Mediterránea denominada “La Cancillería como promotora de las
-                exportaciones argentinas” y que contó con la presencia del ministro de
-                Relaciones Exteriores, Comercio Internacional y Culto de la Nación, como
-                principal orador. Desde CAME creemos que interactuar con el mundo es una
-                gran oportunidad para generar divisas, empleo y riqueza en las pequeñas
-                y medianas empresas argentinas.
+                <a href="http://www.redcame.org.ar/novedades/9794/came-participo-de-la-reunion-la-cancilleria-como-promotora-de-las-exportaciones-argentinas"
+                    target="_blank">Reunión "La Cancillería como promotora de las exportaciones argentinas"</a>
             </p>
 
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10032/came-participo-de-la-mesa-sectorial-binacional"
                 target="_blank">
-                <img src="imgs/gest_pand/03.jpg" alt="" />
+                <img src="imgs/gest_pand/03.jpg" alt="Llevamos a cabo la Mesa Sectrorial Binacional" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                Se llevó adelante la Mesa Sectorial Binacional en el marco del trabajo
-                que la Confederación Argentina de la Mediana Empresa CAME comenzó a
-                desarrollar con entidades sectoriales. Del encuentro participó el
-                presidente de la entidad, Gerardo Díaz Beltrán, y durante su desarrollo,
-                los presentes dialogaron sobre el impacto de la pandemia en el comercio
-                internacional y las falencias operativas locales, altos costos
-                logísticos, demoras en la obtención de licencias no automáticas de
-                importación, reducción de la demanda y dilaciones en la cadena de pago.
-                Con fronteras cerradas y restricciones comerciales la oferta y demanda
-                de productos se vio disminuida afectando el flujo de bienes y servicios.
+                <a href="http://www.redcame.org.ar/novedades/10032/came-participo-de-la-mesa-sectorial-binacional"
+                    target="_blank"> Llevamos a cabo la Mesa Sectrorial Binacional</a>
             </p>
 
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10279/came-se-reunio-con-uatre-para-analizar-medidas-que-beneficien-a-empresarios-y-trabajadores-rurales"
                 target="_blank">
-                <img src="imgs/gest_pand/04.jpg" alt="" />
+                <img src="imgs/gest_pand/04.jpg" alt="Nos reunimos con la UATRE" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                El secretario general de la Unión Argentina de Trabajadores Rurales y
-                Estibadores (UATRE), José Voytenco, se reunió en la sede de Buenos Aires
-                con Alfredo González, secretario gremial de la Confederación Argentina
-                de la Mediana Empresa (CAME), y Jorge Pazos, consejero titular de la
-                entidad, para analizar medidas concretas que beneficien a los
-                trabajadores rurales, sin atentar contra la rentabilidad de los
-                empleadores del sector agroindustrial que, en el mejor de los casos, ya
-                se encuentra en niveles muy bajos.
+                <a href="http://www.redcame.org.ar/novedades/10279/came-se-reunio-con-uatre-para-analizar-medidas-que-beneficien-a-empresarios-y-trabajadores-rurales"
+                    target="_blank"> Nos reunimos con la UATRE</a>
             </p>
 
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10295/agustin-rossi-presento-en-came-el-fondo-nacional-de-la-defensa-fondef"
                 target="_blank">
-                <img src="imgs/gest_pand/07.jpg" alt="" />
+                <img src="imgs/gest_pand/07.jpg" alt="Presentación en CAME del Fondo Nacional de la Defensa (FONDEF)" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                El ministro de Defensa de la Nación, Agustín Rossi, presentó el Fondo
-                Nacional de la Defensa (FONDEF) en una reunión llevada a cabo de manera
-                virtual con una comitiva de dirigentes de la Confederación Argentina de
-                la Mediana Empresa (CAME) de todo el país, encabezada por el presidente
-                de la entidad, Gerardo Díaz Beltrán.
+                <a href="http://www.redcame.org.ar/novedades/10295/agustin-rossi-presento-en-came-el-fondo-nacional-de-la-defensa-fondef"
+                    target="_blank">Presentación en CAME del Fondo Nacional de la Defensa (FONDEF)</a>
             </p>
 
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10316/nos-reunimos-con-la-secretaria-de-planificacion-para-trabajar-en-temas-de-movilidad-y-transporte"
                 target="_blank">
-                <img src="imgs/gest_pand/05.jpg" alt="" />
+                <img src="imgs/gest_pand/05.jpg" alt="Nos reunimos con la Secretaría de Planificación" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                Nos reunimos con la Secretaría de Planificación para trabajar en temas
-                de movilidad y transporte
+                <a href="http://www.redcame.org.ar/novedades/10316/nos-reunimos-con-la-secretaria-de-planificacion-para-trabajar-en-temas-de-movilidad-y-transporte"
+                    target="_blank">Nos reunimos con la Secretaría de Planificación</a>
             </p>
 
         </div>
     </div>
 
-    <div class="portfolio-item">
+    <div class="portfolio-item-bcra">
         <div class="portfolio-image">
             <a href="http://www.redcame.org.ar/novedades/10372/el-fmi-convoco-a-came-para-analizar-la-situacion-de-las-pymes-argentina"
                 target="_blank">
-                <img src="imgs/gest_pand/06.jpg" alt="" />
+                <img src="imgs/gest_pand/06.jpg"
+                    alt="Fuimos convocados por el FMI como referentes de las pymes argentinas" />
             </a>
         </div>
         <div class="portfolio-desc">
             <p class="car_titulo">
-                Dirigentes de la Confederación Argentina de la Mediana Empresa (CAME) se
-                reunieron con una comitiva del Fondo Monetario Internacional (FMI) tras
-                haber sido convocados para plantear las principales problemáticas que
-                aquejan en la actualidad a las pequeñas y medianas empresas y solicitar
-                la inclusión protagónica del sector en la agenda de negociación que
-                tiene el organismo con el Gobierno nacional.
+                <a href="http://www.redcame.org.ar/novedades/10372/el-fmi-convoco-a-came-para-analizar-la-situacion-de-las-pymes-argentina"
+                    target="_blank">Fuimos convocados por el FMI como referentes de las pymes argentinas</a>
             </p>
 
         </div>
